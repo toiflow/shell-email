75c79e418f7270750b856622c6ad6e4d4f8aeeea
jayreck996 <jayreck996@gmail.com>
2026-03-29 17:26:06 +1300

    remove redundant files

--- ought-update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:$PATH"
-
-# run must-file first, then must-email sequentially
-/Users/jayagent/Documents/Github/macmini/must-update_file_content.sh
-/Users/jayagent/Documents/Github/macmini/must-update_email_content.sh
